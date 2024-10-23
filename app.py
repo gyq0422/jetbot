254f14bf3c1b521962768971a7ce60504e9302b9
gyq0422 <1620528919@qq.com>
2024-10-23 22:03:13 +0800

    Initial commit

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import cv2
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+ip = 'rtsp://admin:admin@10.98.160.135:8554/live'
 
 # 初始化视频捕获
-camera = cv2.VideoCapture(0)  # 替换为你的摄像头索引或视频流URL
+camera = cv2.VideoCapture(ip)  # 替换为你的摄像头索引或视频流URL
 
 @app.route('/')
 def index():
@@ -18,11 +19,13 @@ def generate_frames():
         if not success:
             break
         else:
+            print("Frame read successfully")
             # 将图像编码为JPEG
             _, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/video')
 def video():
@@ -35,4 +38,4 @@ def handle_message(data):
     # 这里可以添加控制小车的逻辑
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000,allow_unsafe_werkzeug=True)
---
+++ static/js/car.js
@@ -0,0 +1,1 @@
+document.getElementById('videoFeed').src = '/video';
